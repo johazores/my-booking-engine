bc639f5a8c86fb0ffebdac10aa1cd55d1070c82f
Johanss Azores <johanss.zip@gmail.com>
2023-03-01 16:40:30 +0800

    update approach of api fetches

--- pages/api/search-airports.ts
+++ pages/api/search-airports.ts
@@ -2,28 +2,30 @@ import { NextApiHandler } from 'next';
 
 const handler: NextApiHandler = async (req, res) => {
 
-  if(!req.query.name) {
-    res.status(400).json({ message: 'Name Parameter is Required'});
-    return;
-  }
+  try {
 
-  const config = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': `${process.env.RAPID_API_KEY}`,
-      'X-RapidAPI-Host': `${process.env.RAPID_API_HOST}`
+    if(!req.query.name) {
+      res.status(400).json({ message: 'Name Parameter is Required'});
+      return;
     }
-  };
 
- await fetch(`https://${process.env.RAPID_API_HOST}/airports/search?query=${req.query.name}`, config)
-    .then(response => response.json())
-    .then(response => {
-      res.status(200).json(response);
-    })
-    .catch(err => {
-      res.status(500).json({ err: err });
-      console.error(err)
-    });
+    const config = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': `${process.env.RAPID_API_KEY}`,
+        'X-RapidAPI-Host': `${process.env.RAPID_API_HOST}`
+      }
+    };
+
+    const response = await fetch(`https://${process.env.RAPID_API_HOST}/airports/search?query=${req.query.name}`, config);
+    const data = await response.json();
+
+    res.status(200).json(data);
+
+  } catch(err) {
+    res.status(500).json(err);
+    console.error(err);
+  }
 
   return;
 };
